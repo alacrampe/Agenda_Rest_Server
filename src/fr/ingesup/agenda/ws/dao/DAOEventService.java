f5e9784e0dc17815c31e69b77f2be7e6d4461f3b
Arnaud Lacrampe <arnaud.lacrampe@gmail.com>
2014-01-23 11:53:35 +0100

    Commit pre-pull

---
+++ src/fr/ingesup/agenda/ws/dao/DAOEventService.java
@@ -0,0 +1,35 @@
+package fr.ingesup.agenda.ws.dao;
+
+public class DAOEventService implements DAOService {
+
+	@Override
+	public void getAll(Class objClass) throws DAOException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void save(Object o) throws DAOException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void get(String id) throws DAOException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void update(Object o) throws DAOException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void delete(String id) throws DAOException {
+		// TODO Auto-generated method stub
+
+	}
+
+}
